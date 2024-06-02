4b0a045fbd7d8bf078290f351a036ae8dda017f2
ideoeduardo <ideonapi@gmail.com>
2024-06-01 23:15:05 -0430

    mi-cv-personal
    mi-cv-personal

---
+++ bl-content/databases/users.php
@@ -0,0 +1,24 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{
+    "admin": {
+        "nickname": "Admin",
+        "firstName": "Administrador",
+        "lastName": "",
+        "role": "admin",
+        "password": "9cc5a4471843741430b383205d6d441c8faae3f6",
+        "salt": "665be98f93cc1",
+        "email": "",
+        "registered": "2024-06-01 23:39:59",
+        "tokenRemember": "",
+        "tokenAuth": "ebf492f0cf0c9805dd3c445738afce37",
+        "tokenAuthTTL": "2009-03-15 14:00",
+        "twitter": "",
+        "facebook": "",
+        "instagram": "",
+        "codepen": "",
+        "linkedin": "",
+        "xing": "",
+        "github": "",
+        "gitlab": ""
+    }
+}
